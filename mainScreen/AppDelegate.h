d4b9d31f2bd3fd68795502f157867ae488c4d154
Jake Day <jaked97@gmail.com>
2019-05-05 13:25:20 -0700

    Initial Commit

---
+++ mainScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mainScreen
+//
+//  Created by Jake Day on 5/5/19.
+//  Copyright © 2019 Jake Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mainScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mainScreen
+//
+//  Created by Jake Day on 5/5/19.
+//  Copyright © 2019 Jake Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
